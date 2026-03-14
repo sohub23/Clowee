97a00a7063402275f467e2296a82dfcafc51fe4b
afrozakhanmim-lang <afrozakhanmim@gmail.com>
2026-03-14 17:15:34 +0600

    Change development server port to 8080

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
-    port: 3002,
+    port: 8080,
     strictPort: false,
     hmr: false,
     watch: {
